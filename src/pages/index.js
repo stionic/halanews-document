1edbb32be2284842be44f49fda032e85af66cde2
stionic <admin@stionic.com>
2020-02-11 00:17:38 +0700

    Fix home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import window from 'global';
+
 function Home() {
-  window.location.href = 'docs/introduction';
+  if (window.location) window.location.href = 'docs/introduction';
+  return (
+    <div></div>
+  )
 }
 
 export default Home;
