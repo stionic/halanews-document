1f33bc5becf56324d2353205121099c7815ac930
stionic <admin@stionic.com>
2020-05-12 23:37:46 +0700

    Deploy website version based on 81473d051c193c7c6e283bbaeec037f509e2ce44

--- js/unregister-service-workers.js
+++
@@ -1,5 +0,0 @@
-if (navigator && 'serviceWorker' in navigator) navigator.serviceWorker.getRegistrations().then(function (registrations) {
-  for (let registration of registrations) {
-    registration.unregister();
-  }
-})
