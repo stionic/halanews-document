4c7a5af7fb1c6a52d72b865b4507c05ffb2eb79d
stionic <admin@stionic.com>
2020-07-18 12:45:47 +0700

    Update script create PDF

--- hoge.js
+++ hoge.js
@@ -2,9 +2,10 @@ const puppeteer = require('puppeteer');
 const { PDFRStreamForBuffer, createWriterToModify, PDFStreamForResponse } = require('hummus');
 const { WritableStream } = require('memory-streams');
 const fs = require('fs');
+const config = require('./docusaurus.config');
 
 const mergePdfBlobs = (pdfBlobs) => {
-  const outStream = new WritableStream();                                                                                                                                             
+  const outStream = new WritableStream();
   const [firstPdfRStream, ...restPdfRStreams] = pdfBlobs.map(pdfBlob => new PDFRStreamForBuffer(pdfBlob));
   const pdfWriter = createWriterToModify(firstPdfRStream, new PDFStreamForResponse(outStream));
 
@@ -12,7 +13,7 @@ const mergePdfBlobs = (pdfBlobs) => {
 
   pdfWriter.end();
   outStream.end();
-  
+
   return outStream.toBuffer();
 };
 
@@ -25,8 +26,8 @@ let generatedPdfBlobs = [];
   let nextPageUrl = 'http://localhost:3000/halanews-document/docs/introduction';
 
   while (nextPageUrl) {
-    await page.goto(`${nextPageUrl}`, {waitUntil: 'networkidle2'});
-      
+    await page.goto(`${nextPageUrl}`, { waitUntil: 'networkidle2' });
+
     try {
       nextPageUrl = await page.$eval('.pagination-nav__item--next > a', (element) => {
         return element.href;
@@ -34,17 +35,28 @@ let generatedPdfBlobs = [];
     } catch (e) {
       nextPageUrl = null;
     }
-  
-  
-    let html = await page.$eval('article', (element) => {
+
+    let html = await page.$eval('article', (element, config) => {
+      let i;
+      let aTag = element.querySelectorAll('a');
+      if (aTag) {
+        for (i = 0; i < aTag.length; i++) {
+          if (aTag[i].getAttribute('href') && aTag[i].getAttribute('href').startsWith(config.baseUrl)) {
+            aTag[i].setAttribute('href', config.url + aTag[i].getAttribute('href'));
+          }
+          if (aTag[i].classList.contains('anchor') || aTag[i].classList.contains('hash-link') || !aTag[i].getAttribute('href')) continue;
+          aTag[i].innerText += ' [' + aTag[i].getAttribute('href') + '] ';
+        }
+      }
+
       return element.outerHTML;
-    });
-  
-    
+    }, config);
+
+
     await page.setContent(html);
-    await page.addStyleTag({url: 'http://localhost:3000/halanews-document/styles.css'});
-    await page.addScriptTag({url: 'http://localhost:3000/halanews-document/styles.js'});
-    const pdfBlob = await page.pdf({path: "", format: 'A4', printBackground: true, margin : {top: 20, right: 15, left: 15, bottom: 20}});
+    await page.addStyleTag({ url: 'http://localhost:3000/halanews-document/styles.css' });
+    await page.addScriptTag({ url: 'http://localhost:3000/halanews-document/styles.js' });
+    const pdfBlob = await page.pdf({ path: "", format: 'A4', printBackground: true, margin: { top: 20, right: 15, left: 15, bottom: 20 } });
 
     generatedPdfBlobs.push(pdfBlob);
   }
